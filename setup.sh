fe8f05209326998f9f9bfefe1a5eff30b72a2c78
Martin Alonso <m13alonso@gmail.com>
2017-09-23 00:18:52 +0200

    Add setup script with Rasa installation

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -Ee
+
+ROOT_DIR=$(cd $(dirname $0) && pwd -L)
+
+VENV_DIR="${ROOT_DIR}/.venv"
+
+if [ ! -d ${VENV_DIR} ]; then
+    virtualenv -p python3 ${VENV_DIR}
+fi
+
+source ${VENV_DIR}/bin/activate
+
+
+
+
+# ====== RASA Installation ======
+git clone https://github.com/RasaHQ/rasa_nlu.git
+cd rasa_nlu
+
+# TODO: checkout OS and distribution
+sudo apt-get install -y libblas-dev liblapack-dev
+
+pip install -r requirements.txt
+python setup.py install
+pip install spacy
+python -m spacy download en
+
+pip install scipy
+pip install scikit-learn
+pip install sklearn-crfsuite
+
+cd ${ROOT_DIR}
+
+cp scripts/config_pinkpython_rasa.json rasa_nlu/
+cp -r npl_models/pinkpython-bot/ rasa_nlu/data/
